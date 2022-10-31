e31adb01bc536679a8026b2043fa4bb683cf33a3
阿银 <739178270@qq.com>
2022-10-31 15:56:29 +0800

    page

---
+++ assets/App.48c789f8.js
@@ -0,0 +1,1 @@
+import"./index.a4fd1c5c.js";import{_ as r}from"./index.a4fd1c5c.js";export{r as default};
---
+++ assets/index.1689cc36.js
@@ -0,0 +1,1 @@
+import{d as o,a,o as n,c as s,w as l,b as r}from"./index.a4fd1c5c.js";const d=o({__name:"index",setup(_){return(e,c)=>{const t=a("el-tag");return n(),s(t,null,{default:l(()=>[r(e.$slots,"default")]),_:3})}}});export{d as default};
